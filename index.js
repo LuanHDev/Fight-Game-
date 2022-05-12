3c2531a1be98f0c6927fb59db093ca1d7c118848
LuanHDev <luanh17@gmail.com>
2022-05-11 21:53:36 -0300

    Movimento player e inimigo

--- index.js
+++ index.js
@@ -8,26 +8,30 @@ c.fillRect(0, 0, canvas.width, canvas.height);
 
 const gravity = 0.2
 
-class Sprite{
-    constructor({position, velocity}){
+class Sprite {
+    constructor({ position, velocity }) {
         this.position = position
-        this.velocity= velocity
+        this.velocity = velocity
         this.height = 150
+        this.lastKey
     }
-    
+
     draw() {
-        c.fillStyle= 'red'
+        c.fillStyle = 'red'
         c.fillRect(this.position.x, this.position.y, 50, this.height)
     }
 
-    update(){
+    update() {
         this.draw()
-        
-        this.position.y= this.position.y + this.velocity.y
-        
-        if(this.position.y + this.height + this.velocity.y>= canvas.height) {
-            this.velocity.y= 0
-        }else this.velocity.y += gravity
+
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+
+        this.position.y = this.position.y + this.velocity.y
+
+        if (this.position.y + this.height + this.velocity.y >= canvas.height) {
+            this.velocity.y = 0
+        } else this.velocity.y += gravity
     }
 }
 
@@ -36,8 +40,8 @@ const player = new Sprite({
     position: {
         x: 0,
         y: 0
-    }, 
-    velocity:{
+    },
+    velocity: {
         x: 0,
         y: 0
     }
@@ -45,11 +49,11 @@ const player = new Sprite({
 
 
 const enemy = new Sprite({
-    position : {
+    position: {
         x: 400,
         y: 100
-    }, 
-    velocity :{
+    },
+    velocity: {
         x: 0,
         y: 0
     }
@@ -59,13 +63,111 @@ const enemy = new Sprite({
 
 console.log(player);
 
+const keys = {
+    a: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    },
+    w: {
+        pressed: false
+    },
+    ArrowRight: {
+        pressed: false
+    },
+    ArrowLeft: {
+        pressed: false
+    },
+    ArrowUp: {
+        pressed: false
+    }
+}
 
-function animate(){
+
+function animate() {
     window.requestAnimationFrame(animate)
     c.fillStyle = 'black';
     c.fillRect(0, 0, canvas.width, canvas.height)
     player.update()
     enemy.update()
+
+    //Player Moviment
+    player.velocity.x = 0
+    if (keys.a.pressed && player.lastKey === 'a') {
+        player.velocity.x = -1
+    } else if (keys.d.pressed && player.lastKey === 'd') {
+        player.velocity.x = 1
+    }
+
+    //Enemy Moviment
+    enemy.velocity.x= 0
+    if (keys.ArrowLeft.pressed && enemy.lastKey === 'ArrowLeft') {
+        enemy.velocity.x = -1
+    } else if (keys.ArrowRight.pressed && enemy.lastKey === 'ArrowRight') {
+        enemy.velocity.x = 1
+    }
 }
 
 animate()
+
+window.addEventListener('keydown', (event) => {
+    switch (event.key) {
+        case 'd':
+            keys.d.pressed = true
+            player.lastKey = 'd'
+            break
+        case 'a':
+            keys.a.pressed = true
+            player.lastKey = 'a'
+            break
+        case 'w':
+            keys.w.pressed = true
+            player.velocity.y = -7
+            break
+
+        case 'ArrowRight':
+            keys.ArrowRight.pressed = true
+            enemy.lastKey = 'ArrowRight'
+            break
+        case 'ArrowLeft':
+            keys.ArrowLeft.pressed = true
+            enemy.lastKey = 'ArrowLeft'
+            break
+        case 'ArrowUp':
+            enemy.velocity.y = -7
+            break
+
+    }
+    console.log(event.key)
+})
+
+window.addEventListener('keyup', (event) => {
+    switch (event.key) {
+        case 'd':
+            keys.d.pressed = false
+            break
+        case 'a':
+            keys.a.pressed = false
+            break
+        case 'w':
+            keys.w.pressed = false
+            break
+    }
+
+    switch (event.key) {
+        case 'ArrowRight':
+            keys.ArrowRight.pressed = false
+            break
+        case 'ArrowLeft':
+            keys.ArrowLeft.pressed = false
+            break
+        case 'ArrowUP':
+            keys.ArrowUp.pressed = false
+            break
+    }
+
+    console.log(event.key)
+
+})
+
